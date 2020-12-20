a889198dce6a4066ccc51ac9f55809aa3dac6015
kamoto0427 <takahiro.08039@gmail.com>
2020-12-20 11:38:06 +0900

    フォームの見た目

--- src/index.js
+++ src/index.js
@@ -2,4 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Home from './components/Home.jsx';
 
-ReactDOM.render(<Home />, document.getElementByld('root'));
+ReactDOM.render(<Home />, document.getElementById('root'));
